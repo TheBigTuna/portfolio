595535b6f4ca1cb07cd56f1b0aa7d29e9c8a47ce
octavius <mooreoctavius94@gmail.com>
2020-01-14 19:25:28 -0500

    update 1/11/2020
    update 1/11/2020

--- assets/js/Components/Projects.js
+++ assets/js/Components/Projects.js
@@ -13,52 +13,52 @@ export default class Projects extends React.Component{
     }
 
     iconDisplay() { 
-      let hB1 = document.getElementById('hoverBox1');  
-      let hB2 = document.getElementById('hoverBox2');  
+      // let hB1 = document.getElementById('hoverBox1');  
+      // let hB2 = document.getElementById('hoverBox2');  
       // let hB3 = document.getElementById('hoverBox3');  
       // let hB4 = document.getElementById('hoverBox4');  
-      let hB5 = document.getElementById('hoverBox5');  
-      let hB6 = document.getElementById('hoverBox6'); 
-      let p1 = document.getElementById('p1');  
-      let p2 = document.getElementById('p2');  
+      // let hB5 = document.getElementById('hoverBox5');  
+      // let hB6 = document.getElementById('hoverBox6'); 
+      // let p1 = document.getElementById('p1');  
+      // let p2 = document.getElementById('p2');  
       // let p3 = document.getElementById('p3');  
       // let p4 = document.getElementById('p4');  
-      let p5 = document.getElementById('p5');  
-      let p6 = document.getElementById('p6'); 
-      let d1 = document.getElementById('display1');
-      let d2 = document.getElementById('display2');
+      // let p5 = document.getElementById('p5');  
+      // let p6 = document.getElementById('p6'); 
+      // let d1 = document.getElementById('display1');
+      // let d2 = document.getElementById('display2');
       // let d3 = document.getElementById('display3');
-      let d4 = document.getElementById('display4');
-      let d5 = document.getElementById('display5');
-      let d6 = document.getElementById('display6');
+      // let d4 = document.getElementById('display4');
+      // let d5 = document.getElementById('display5');
+      // let d6 = document.getElementById('display6');
 
-      p1.addEventListener('mouseover', () =>{hB1.style.display = 'block'; d1.setAttribute("class", "Animated flipInX");});
-      p2.addEventListener('mouseover', () =>{hB2.style.display = 'block'; d2.setAttribute("class", "Animated flipInX");});
+      // p1.addEventListener('mouseover', () =>{hB1.style.display = 'block'; d1.setAttribute("class", "Animated flipInX");});
+      // p2.addEventListener('mouseover', () =>{hB2.style.display = 'block'; d2.setAttribute("class", "Animated flipInX");});
       // p3.addEventListener('mouseover', () =>{hB3.style.display = 'block'; d3.setAttribute("class", "Animated flipInX");});
       // p4.addEventListener('mouseover', () =>{hB4.style.display = 'block'; d4.setAttribute("class", "Animated flipInX");});
-      p5.addEventListener('mouseover', () =>{hB5.style.display = 'block'; d5.setAttribute("class", "Animated flipInX");});
-      p6.addEventListener('mouseover', () =>{hB6.style.display = 'block'; d6.setAttribute("class", "Animated flipInX");});
+      // p5.addEventListener('mouseover', () =>{hB5.style.display = 'block'; d5.setAttribute("class", "Animated flipInX");});
+      // p6.addEventListener('mouseover', () =>{hB6.style.display = 'block'; d6.setAttribute("class", "Animated flipInX");});
 
     }
     iconHide() { 
-      let hB1 = document.getElementById('hoverBox1');  
-      let hB2 = document.getElementById('hoverBox2');  
+      // let hB1 = document.getElementById('hoverBox1');  
+      // let hB2 = document.getElementById('hoverBox2');  
       // let hB3 = document.getElementById('hoverBox3');  
       // let hB4 = document.getElementById('hoverBox4');  
-      let hB5 = document.getElementById('hoverBox5');  
-      let hB6 = document.getElementById('hoverBox6'); 
-      let p1 = document.getElementById('p1');  
-      let p2 = document.getElementById('p2');  
+      // let hB5 = document.getElementById('hoverBox5');  
+      // let hB6 = document.getElementById('hoverBox6'); 
+      // let p1 = document.getElementById('p1');  
+      // let p2 = document.getElementById('p2');  
       // let p3 = document.getElementById('p3');  
       // let p4 = document.getElementById('p4');  
-      let p5 = document.getElementById('p5');  
-      let p6 = document.getElementById('p6'); 
-      p1.addEventListener('mouseleave', () =>{hB1.style.display = 'none';});  
-      p2.addEventListener('mouseleave', () =>{hB2.style.display = 'none';});  
+      // let p5 = document.getElementById('p5');  
+      // let p6 = document.getElementById('p6'); 
+      // p1.addEventListener('mouseleave', () =>{hB1.style.display = 'none';});  
+      // p2.addEventListener('mouseleave', () =>{hB2.style.display = 'none';});  
       // p3.addEventListener('mouseleave', () =>{hB3.style.display = 'none';});  
       // p4.addEventListener('mouseleave', () =>{hB4.style.display = 'none';});  
-      p5.addEventListener('mouseleave', () =>{hB5.style.display = 'none';}); 
-      p6.addEventListener('mouseleave', () =>{hB6.style.display = 'none';}); 
+      // p5.addEventListener('mouseleave', () =>{hB5.style.display = 'none';}); 
+      // p6.addEventListener('mouseleave', () =>{hB6.style.display = 'none';}); 
     }
     
     render(){
@@ -132,7 +132,7 @@ export default class Projects extends React.Component{
                 </span>
               </span>
           </div>
-             {/* <div className="col-lg-6">
+             <div className="col-lg-6">
             <a className="portfolio-item" id="p6" href="https://thebigtuna.github.io/DetDigitalAgency/" target="_blank" onMouseEnter={this.iconDisplay}  onMouseLeave={this.iconHide}>
             <div className= "hoverBox" id="hoverBox6">
                <h4 id="display6">Display<i className="fa fa-arrow-right" aria-hidden="true"></i></h4>
@@ -147,14 +147,14 @@ export default class Projects extends React.Component{
                   <i className="devicon-css3-plain-wordmark colored pIcon" />
                   <i className="devicon-javascript-plain colored pIcon" />
                   <i className="devicon-jquery-plain-wordmark colored pIcon" />
-                  <i className="devicon-php-plain colored pIcon"/>
-                  <i className="devicon-mysql-plain-wordmark pIcon" />
+                  {/* <i className="devicon-php-plain colored pIcon"/> */}
+                  {/* <i className="devicon-mysql-plain-wordmark pIcon" /> */}
                   <i className="devicon-git-plain-wordmark colored pIcon" />
                   </p>
                 </span>
               </span>
           </div>
-            */}
+           
           {/* <div className="col-lg-6">
             <a className="portfolio-item"  id="p3" href="http://sybeq.com/html/home.html" target="_blank" onMouseEnter={this.iconDisplay}  onMouseLeave={this.iconHide}>
              <div className= "hoverBox" id="hoverBox3">
@@ -196,7 +196,7 @@ export default class Projects extends React.Component{
                 </span>
               </span>
           </div> */}
-          {/* <div className="col-lg-6">
+          <div className="col-lg-6">
             <a className="portfolio-item"  id="p4" href="https://octaviusmoore.com/SkateKicks/" target="_blank" onMouseEnter={this.iconDisplay}  onMouseLeave={this.iconHide}>
              <div className= "hoverBox" id="hoverBox4">
                <h4 id="display4">Display<i className="fa fa-arrow-right" aria-hidden="true"></i></h4>
@@ -208,12 +208,12 @@ export default class Projects extends React.Component{
                   <h2 className="pName mt-1">Skatekicks Website</h2>
                   <p className="mb-0">
                   <i className="devicon-wordpress-plain-wordmark colored pIcon" />
-                  <a href="https://github.com/TheBigTuna/dylantribute" target="_blank"><button className="viewCode btn">View Code</button></a>
+                  {/* <a href="https://github.com/TheBigTuna/dylantribute" target="_blank"><button className="viewCode btn">View Code</button></a> */}
                   </p>
                 </span>
               </span>
-          </div> */}
-            <div className="col-lg-6">
+          </div>
+            {/* <div className="col-lg-6">
             <a className="portfolio-item" id="p5" href="https://alleuro.com" target="_blank" onMouseEnter={this.iconDisplay}  onMouseLeave={this.iconHide}>
               <div className= "hoverBox" id="hoverBox5">
                <h4 id="display5">Display<i className="fa fa-arrow-right" aria-hidden="true"></i></h4>
@@ -231,11 +231,11 @@ export default class Projects extends React.Component{
                   <i className="devicon-php-plain colored pIcon"/>
                   <i className="devicon-mysql-plain-wordmark pIcon" />
                   <i className="devicon-git-plain-wordmark colored pIcon" />
-                  {/*<a href="https://github.com/TheBigTuna/youtubeplayer" target="_blank"><button className="viewCode btn">View Code</button></a>*/}
+                  <a href="https://github.com/TheBigTuna/youtubeplayer" target="_blank"><button className="viewCode btn">View Code</button></a>
                   </p>
                 </span>
               </span>
-          </div>
+          </div> */}
           
            {/*<div className="col-lg-6">
             <a className="portfolio-item" href="#" target="_blank">
